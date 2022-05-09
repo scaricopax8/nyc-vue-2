00c59185f2cb3646a009fe12faa3cad747657e2f
scaricopax8 <50639783+scaricopax8@users.noreply.github.com>
2022-05-09 10:10:10 -0600

    initial commit

---
+++ instrumented/main.js
@@ -0,0 +1,108 @@
+function cov_1hmxq5hqw4() {
+  var path = "/Users/pax8-scarico-02/Development/nyc-vue-2/src/main.js";
+  var hash = "5ec57afa1bb87c58a9731f2c4b5f7953a44912c3";
+  var global = new Function("return this")();
+  var gcv = "__coverage__";
+  var coverageData = {
+    path: "/Users/pax8-scarico-02/Development/nyc-vue-2/src/main.js",
+    statementMap: {
+      "0": {
+        start: {
+          line: 4,
+          column: 0
+        },
+        end: {
+          line: 4,
+          column: 32
+        }
+      },
+      "1": {
+        start: {
+          line: 6,
+          column: 0
+        },
+        end: {
+          line: 8,
+          column: 17
+        }
+      },
+      "2": {
+        start: {
+          line: 7,
+          column: 15
+        },
+        end: {
+          line: 7,
+          column: 21
+        }
+      }
+    },
+    fnMap: {
+      "0": {
+        name: "(anonymous_0)",
+        decl: {
+          start: {
+            line: 7,
+            column: 10
+          },
+          end: {
+            line: 7,
+            column: 11
+          }
+        },
+        loc: {
+          start: {
+            line: 7,
+            column: 15
+          },
+          end: {
+            line: 7,
+            column: 21
+          }
+        },
+        line: 7
+      }
+    },
+    branchMap: {},
+    s: {
+      "0": 0,
+      "1": 0,
+      "2": 0
+    },
+    f: {
+      "0": 0
+    },
+    b: {},
+    _coverageSchema: "1a1c01bbd47fc00a2c39e90264f33305004495a9",
+    hash: "5ec57afa1bb87c58a9731f2c4b5f7953a44912c3"
+  };
+  var coverage = global[gcv] || (global[gcv] = {});
+
+  if (!coverage[path] || coverage[path].hash !== hash) {
+    coverage[path] = coverageData;
+  }
+
+  var actualCoverage = coverage[path];
+  {
+    // @ts-ignore
+    cov_1hmxq5hqw4 = function () {
+      return actualCoverage;
+    };
+  }
+  return actualCoverage;
+}
+
+cov_1hmxq5hqw4();
+import Vue from 'vue';
+import App from './App.vue';
+cov_1hmxq5hqw4().s[0]++;
+Vue.config.productionTip = false;
+cov_1hmxq5hqw4().s[1]++;
+new Vue({
+  render: h => {
+    cov_1hmxq5hqw4().f[0]++;
+    cov_1hmxq5hqw4().s[2]++;
+    return h(App);
+  }
+}).$mount('#app');
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJuYW1lcyI6WyJWdWUiLCJBcHAiLCJjb25maWciLCJwcm9kdWN0aW9uVGlwIiwicmVuZGVyIiwiaCIsIiRtb3VudCJdLCJzb3VyY2VzIjpbIm1haW4uanMiXSwic291cmNlc0NvbnRlbnQiOlsiaW1wb3J0IFZ1ZSBmcm9tICd2dWUnXG5pbXBvcnQgQXBwIGZyb20gJy4vQXBwLnZ1ZSdcblxuVnVlLmNvbmZpZy5wcm9kdWN0aW9uVGlwID0gZmFsc2VcblxubmV3IFZ1ZSh7XG4gIHJlbmRlcjogaCA9PiBoKEFwcCksXG59KS4kbW91bnQoJyNhcHAnKVxuIl0sIm1hcHBpbmdzIjoiOzs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7OztJQWVZOzs7Ozs7Ozs7QUFmWixPQUFPQSxHQUFQLE1BQWdCLEtBQWhCO0FBQ0EsT0FBT0MsR0FBUCxNQUFnQixXQUFoQjs7QUFFQUQsR0FBRyxDQUFDRSxNQUFKLENBQVdDLGFBQVgsR0FBMkIsS0FBM0I7O0FBRUEsSUFBSUgsR0FBSixDQUFRO0VBQ05JLE1BQU0sRUFBRUMsQ0FBQyxJQUFJO0lBQUE7SUFBQTtJQUFBLE9BQUFBLENBQUMsQ0FBQ0osR0FBRCxDQUFEO0VBQU07QUFEYixDQUFSLEVBRUdLLE1BRkgsQ0FFVSxNQUZWIn0=
